d4dbfd962aa3b9ab37610eb478916ea5ee727862
Marko Savić <marko.savic@ugbb.net>
2024-01-21 19:05:30 +0100

    adding additional packages.Setting them up.Adding the src folders like, routes, middleware, router, helpers and db

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,13 +1,50 @@
 // src/index.ts
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import http from 'http'
 
+
+
+// #region Setup
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, this is your Express app with TypeScript!');
+dotenv.config();
+const MONGO_URL_LOCAL = process.env.MONGO_URL_LOCAL
+
+app.use(cors({
+  credentials: true
+}))
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+// #endregion
+
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+
+// Mongoose
+mongoose.Promise = Promise;
+if (MONGO_URL_LOCAL) {
+  mongoose.connect(MONGO_URL_LOCAL);
+  mongoose.connection.on('error', (error: Error) => {
+    console.log(error)
+})
+} else {
+  console.error('MongoDB URL is undefined. Connection not established.');
+}
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, this is your Express app with TypeScript!');
 });
